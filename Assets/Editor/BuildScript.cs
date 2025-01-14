e99e4e2ed91b976a99fa8441a412eb780e0349d5
Antoine Bodin <antoine-bodin@hotmail.fr>
2025-01-14 12:47:59 +0100

    feat: Add github pages deploy workflow
    Add workflow yaml file
    
    Add Build flder to .gitignore
    
    Add build script

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildScript
+{
+    [MenuItem("Tools/Build WebGL")]
+    public static void BuildWebGL()
+    {
+        string buildPath = "Build/WebGL";
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/MainGame.unity" }, // Remplace par ta scène principale
+            locationPathName = buildPath,
+            target = BuildTarget.WebGL,
+            options = BuildOptions.None
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+        else if (summary.result == BuildResult.Failed)
+        {
+            Debug.LogError("Build failed");
+        }
+    }
+}
